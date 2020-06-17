bcfe202afd29e2aa5315b5ef09fbce2b6921c631
bipon68 <bipon68@users.noreply.github.com>
2020-06-17 13:25:37 +0600

    set value showing useing unordered

--- set.cpp
+++ set.cpp
@@ -6,23 +6,19 @@ int main(){
         You are give N integers. Find the number of distinct integers among them;
         9
         1 2 1 2 3 1 2 2 3
+        // self balancing binary search tree
     */
-    set<int>my_set; // item gulo ascending order e sorted thake
+    unordered_set<int>my_set; // item gulo ascending order e sorted thake
+    int n;
+    cin >> n; // equivalent to scanf("%d", &n);
+    for(int i = 0; i < n; i++){
+        int val;
+        cin >> val;
+        my_set.insert(val);
+    }
+    for(int val: my_set) cout << val << " ";
+    //cout << my_set.size() << "\n"; // print("%d\n", my_set.size());
 
-    my_set.insert(1);
-    my_set.insert(2);
-    my_set.insert(1);
-    my_set.insert(2);
-    my_set.insert(3);
-    for(auto val: my_set)
-        cout << val << " ";
-    return 0;
-    /*
-    my_set.insert(1) << "\n";
-    my_set.insert(2) << "\n";
-    my_set.insert(2) << "\n";
-    my_set.insert(3) << "\n";
-    */
 
 
     return 0;
