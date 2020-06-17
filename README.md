9dad3122f8131f5846645acd57e5e46352ca1e45
bipon68 <bipon68@users.noreply.github.com>
2020-06-17 13:05:14 +0600

    set initial

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Competive Programming Practice
 
+## Reference
 [C++ STL :: vector](https://zobayer2009.wordpress.com/)
+[Vecotr](http://www.cplusplus.com/reference/vector/vector/)
+
 
 
--- set.cpp
+++ set.cpp
@@ -1,7 +1,29 @@
 #include<bits/stdc++.h>
-using namespace;
+using namespace std;
 
 int main(){
+    /*
+        You are give N integers. Find the number of distinct integers among them;
+        9
+        1 2 1 2 3 1 2 2 3
+    */
+    set<int>my_set; // item gulo ascending order e sorted thake
+
+    my_set.insert(1);
+    my_set.insert(2);
+    my_set.insert(1);
+    my_set.insert(2);
+    my_set.insert(3);
+    for(auto val: my_set)
+        cout << val << " ";
+    return 0;
+    /*
+    my_set.insert(1) << "\n";
+    my_set.insert(2) << "\n";
+    my_set.insert(2) << "\n";
+    my_set.insert(3) << "\n";
+    */
+
 
     return 0;
 }
