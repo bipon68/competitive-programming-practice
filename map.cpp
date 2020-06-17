434164f5e47833a3665613e4bfc120428c784997
bipon68 <bipon68@users.noreply.github.com>
2020-06-17 14:37:32 +0600

    map string

--- map.cpp
+++ map.cpp
@@ -7,17 +7,21 @@ int main(){
         2. huge index!
         3. index er range unknown!
     */
-    map<int, int>mp; // <key_type, value_type>
-    mp[-51] = 3;
-    mp[23123] = 4;
-    mp[10] = 5;
+    // int, string, double
 
-    if(mp.find(-5) != mp.end()){
+    map<string, int>mp; // <key_type, value_type>
+    mp["Bipon"] = 55;
+    mp["Arif"] = 100;
+    mp["Shazol"] = 110;
+
+    cout << mp["Arif"] << "\n";
+
+    /*if(mp.find(-5) != mp.end()){
         cout << "Khuje paisi " << "\n"; // stream flush with endl in c++
     }
     if(mp.find(-5) == mp.end()){
         cout << "Khuje pai nai! " << "\n"; // stream flush with endl in c++
-    }
+    }*/
     //cout << mp[-5] << "\n";
 
     return 0;
